9eb468c920d4fb5454e9c2e4fc9aa284ad2ddcff
MarioRaid99 <33650221+MarioRaid99@users.noreply.github.com>
2025-05-24 21:08:22 +0300

    Made City Modal and Fixed Issues

--- Controllers/CityController1.cs
+++ Controllers/CityController1.cs
@@ -80,15 +80,6 @@ namespace AdvancedAjax.Controllers
 
         [ValidateAntiForgeryToken]
         [HttpPost]
-        public IActionResult Delete(City City)
-        {
-            _context.Attach(City);
-            _context.Entry(City).State = EntityState.Deleted;
-            _context.SaveChanges();
-            return RedirectToAction(nameof(Index));
-        }
-
-
         private List<SelectListItem> GetCountries()
         {
             var lstCountries = new List<SelectListItem>();
@@ -112,5 +103,33 @@ namespace AdvancedAjax.Controllers
             return lstCountries;
         }
 
+        [HttpGet]
+        public IActionResult CreateModalForm(int countryId)
+        {
+            City city = new City();
+            city.CountryId = countryId;
+            city.CountryName = GetCountryName(countryId);
+            return PartialView("_CreateModalForm", city);
+        }
+
+        [HttpPost]
+        public IActionResult CreateModalForm(City city)
+        {
+            _context.Add(city);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        private string GetCountryName(int countryId)
+        {
+            if (countryId == 0)
+                return "";
+
+            string strCountryName = _context.Countries
+                .Where(ct => ct.Id == countryId)
+                .Select(nm => nm.Name).Single().ToString();
+
+            return strCountryName;
+        }
     }
 }
